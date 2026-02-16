143e6a27d251c0183f211ecf297b474cda4093c7
VadimFilips <sidramones775@gmail.com>
2026-02-17 01:55:42 +0300

    Add files via upload

---
+++ Задание №2/data_loader.py
@@ -0,0 +1,11 @@
+# data_loader.py
+def load_csv(file_path):
+    """
+    Загрузка данных из CSV файла
+    """
+    try:
+        data = pd.read_csv(file_path)
+        return data
+    except Exception as e:
+        print(f"Ошибка загрузки CSV: {e}")
+        return None
---
+++ Задание №2/ml_module.py
@@ -0,0 +1,32 @@
+#ml_module.py
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+
+
+def train_model(X, y):
+    """
+    Обучение модели линейной регрессии.
+    """
+    model = LinearRegression()
+    model.fit(X, y)
+    return model
+
+def predict(model, X):
+    """
+    Предсказание на новых данных.
+    """
+    return model.predict(X)
+
+def evaluate_model(y_true, y_pred):
+    """
+    Оценка модели.
+    """
+    mse = mean_squared_error(y_true, y_pred)
+    r2 = r2_score(y_true, y_pred)
+    return mse, r2
